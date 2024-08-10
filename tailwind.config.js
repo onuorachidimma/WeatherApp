b9d7331e53e6a4b396f2d95249ab31aaeecfa3ea
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-10 03:29:37 +0100

    change favicon, customize tailwind style, add svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+      },
+    },
   },
   plugins: [],
 }
-
